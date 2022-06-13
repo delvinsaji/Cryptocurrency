a036f329e2424c4c5c73f72c3316acc1abdf21e1
delvinsaji <91379593+delvinsaji@users.noreply.github.com>
2022-06-13 11:28:50 +0300

    header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,7 +2,19 @@ import React from "react";
 import "./Header.css";
 
 function Header() {
-  return <div></div>;
+  return (
+    <div className="header">
+      <div className="headername">
+        <h1 className="header1">CRYPTO</h1>
+        <h1 className="header2"> CURRENCY</h1>
+      </div>
+      <div className="headerlinks">
+        <p>Home</p>
+        <p>News</p>
+        <p>Top Cryptos</p>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
